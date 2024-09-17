9278d9d65895574db618a2b04de8925b3b1fc2af
Mikkel-Boje <mikkel.larsen@stofanet.dk>
2024-09-17 13:51:32 +0200

    Started working on the client CLI

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,6 @@
 use hello_world::greeter_client::GreeterClient;
 use hello_world::HelloRequest;
+use std::io;
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -18,4 +19,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("RESPONSE={:?}", response);
 
     Ok(())
+}
+
+fn login() {
+    let mut username = String::new();
+
+    println!("Welcome, please enter your username");
+
+    match io::stdin().read_line(&mut username) {
+        Err(error) => {
+            println!("Username fmt error, {}", error);
+            todo!()
+        }
+        Ok(_) => {}
+    }
+
+    let mut password = String::new();
+    let set_password = false;
+    let tries = 3; // Can be a changed later if this needs to be tied to the server in anyway
+                   // for security
+
+    while (tries > 0) && !set_password {
+        match io::stdin().read_line(&mut password) {
+            Err(error) => {
+                println!("{}", error);
+                continue;
+            }
+            Ok(_) => {
+                todo!() // Check on the server if the password is correct, if yes, change the
+                        // set_password to true, else, continue and let
+                        // the user try again
+            }
+        }
+    }
 }
