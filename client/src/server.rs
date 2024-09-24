8e93de0dc57608e08e8b34cd0089d9aea981474c
alexsteffensen <asteff20@student.aau.dk>
2024-09-24 11:13:26 +0200

    implemented server trait
    # Conflicts:
    #	client/src/server.rs

--- client/src/server.rs
+++ client/src/server.rs
@@ -1,3 +1,56 @@
 use libsignal_protocol::*;
+use common::signal_protobuf::Envelope;
+use crate::contact::{Contact, Device};
 
-pub trait Server {}
+struct ServerAPI {
+    // data
+}
+
+pub trait Server {
+    async fn publish_bundle(
+        &self,
+        registration_id: u32,
+        bundle: &PreKeyBundle,
+    ) -> Result<(), String>; // should take keys as parameter or struct
+    async fn fetch_bundle(&self, contact: &Contact) -> Result<PreKeyBundle, String>;
+    async fn register_client(&self, client_info: &Contact) -> Result<Ok(), Err()>;
+    async fn register_device(&self, device: &Device) -> Result<Ok(), Err()>;
+    async fn send_msg(&self, envelope: Envelope) -> Result<Ok(), Err()>;
+    async fn update_client(&self, new_client: &Contact) -> Result<Ok(), Err()>;
+    async fn delete_client(&self, contact: &Contact) -> Result<Ok(), Err()>;
+    async fn delete_device(&self, device: &Device) -> Result<Ok(), Err()>;
+}
+
+impl Server for ServerAPI {
+    async fn publish_bundle(&self, registration_id: u32, bundle: &PreKeyBundle) -> Result<(), String> {
+        todo!()
+    }
+
+    async fn fetch_bundle(&self, contact: &Contact) -> Result<PreKeyBundle, String> {
+        todo!()
+    }
+
+    async fn register_client(&self, client_info: &Contact) -> Result<Ok(), Err()> {
+        todo!()
+    }
+
+    async fn register_device(&self, device: &Device) -> Result<Ok(), Err()> {
+        todo!()
+    }
+
+    async fn send_msg(&self, envelope: Envelope) -> Result<Ok(), Err()> {
+        todo!()
+    }
+
+    async fn update_client(&self, new_client: &Contact) -> Result<Ok(), Err()> {
+        todo!()
+    }
+
+    async fn delete_client(&self, contact: &Contact) -> Result<Ok(), Err()> {
+        todo!()
+    }
+
+    async fn delete_device(&self, device: &Device) -> Result<Ok(), Err()> {
+        todo!()
+    }
+}
