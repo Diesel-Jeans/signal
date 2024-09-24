fdc9d48f89f74b2aa02b458b6c5f31c4aab267b9
alexsteffensen <asteff20@student.aau.dk>
2024-09-24 12:25:27 +0200

    removed return types as of now

--- client/src/server.rs
+++ client/src/server.rs
@@ -1,4 +1,5 @@
 use libsignal_protocol::*;
+use surf::Url;
 use common::signal_protobuf::Envelope;
 use crate::contact::{Contact, Device};
 
@@ -7,22 +8,29 @@ struct ServerAPI {
 }
 
 pub trait Server {
+    async fn connect();
     async fn publish_bundle(
         &self,
         registration_id: u32,
         bundle: &PreKeyBundle,
     ) -> Result<(), String>; // should take keys as parameter or struct
     async fn fetch_bundle(&self, contact: &Contact) -> Result<PreKeyBundle, String>;
-    async fn register_client(&self, client_info: &Contact) -> Result<Ok(), Err()>;
-    async fn register_device(&self, device: &Device) -> Result<Ok(), Err()>;
-    async fn send_msg(&self, envelope: Envelope) -> Result<Ok(), Err()>;
-    async fn update_client(&self, new_client: &Contact) -> Result<Ok(), Err()>;
-    async fn delete_client(&self, contact: &Contact) -> Result<Ok(), Err()>;
-    async fn delete_device(&self, device: &Device) -> Result<Ok(), Err()>;
-    async fn connect() -> Result<dyn Server, Err()>;
+    async fn register_client(&self, client_info: &Contact);
+    async fn register_device(&self, device: &Device);
+    async fn send_msg(&self, envelope: Envelope);
+    async fn update_client(&self, new_client: &Contact);
+    async fn delete_client(&self, contact: &Contact);
+    async fn delete_device(&self, device: &Device);
 }
 
 impl Server for ServerAPI {
+    async fn connect(){
+        let server_url = Url::parse("ws://127.0.0.1:50051").unwrap();
+
+
+
+
+    }
     async fn publish_bundle(&self, registration_id: u32, bundle: &PreKeyBundle) -> Result<(), String> {
         todo!()
     }
@@ -31,31 +39,29 @@ impl Server for ServerAPI {
         todo!()
     }
 
-    async fn register_client(&self, client_info: &Contact) -> Result<Ok(), Err()> {
+    async fn register_client(&self, client_info: &Contact){
         todo!()
     }
 
-    async fn register_device(&self, device: &Device) -> Result<Ok(), Err()> {
+    async fn register_device(&self, device: &Device) {
         todo!()
     }
 
-    async fn send_msg(&self, envelope: Envelope) -> Result<Ok(), Err()> {
+    async fn send_msg(&self, envelope: Envelope){
         todo!()
     }
 
-    async fn update_client(&self, new_client: &Contact) -> Result<Ok(), Err()> {
+    async fn update_client(&self, new_client: &Contact) {
         todo!()
     }
 
-    async fn delete_client(&self, contact: &Contact) -> Result<Ok(), Err()> {
+    async fn delete_client(&self, contact: &Contact) {
         todo!()
     }
 
-    async fn delete_device(&self, device: &Device) -> Result<Ok(), Err()> {
+    async fn delete_device(&self, device: &Device) {
         todo!()
     }
 
-    async fn connect() -> Result<dyn Server, Err()> {
-        todo!()
-    }
+
 }
