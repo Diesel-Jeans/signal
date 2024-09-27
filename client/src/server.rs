ecb0b9e6c341222088baca638701229f7fae4ae9
alexsteffensen <asteff20@student.aau.dk>
2024-09-27 12:38:57 +0200

    arguments in uri added

--- client/src/server.rs
+++ client/src/server.rs
@@ -2,13 +2,14 @@ use crate::contact::{Contact, Device};
 use common::signal_protobuf::Envelope;
 use http::StatusCode;
 use libsignal_protocol::*;
+use std::fmt::format;
 use std::time::Duration;
 use surf::http::convert::json;
 use surf::{http, Client, Config, Response, Url};
 use tokio_tungstenite::connect_async;
 
 const CLIENT_URI: &str = "/client";
-const MSG_URI: &str = "/messages";
+const MSG_URI: &str = "/message";
 const DEVICE_URI: &str = "/device";
 const BUNDLE_URI: &str = "/bundle";
 
@@ -26,22 +27,30 @@ enum ReqType {
 pub trait Server {
     async fn connect();
     async fn publish_bundle(
-        &self, //registration_id: u32,
-               //bundle: &PreKeyBundle,
-    ) -> Result<(), Box<dyn std::error::Error>>; // should take keys as parameter or struct
-    async fn fetch_bundle(&self) -> Result<(), Box<dyn std::error::Error>>;
+        &self,
+        uuid: String, //registration_id: u32,
+                      //bundle: &PreKeyBundle,
+    ) -> Result<Response, Box<dyn std::error::Error>>; // should take keys as parameter or struct
+    async fn fetch_bundle(&self, uuid: String) -> Result<Response, Box<dyn std::error::Error>>;
     async fn register_client(
         &self,
         client_info: &Contact,
-    ) -> Result<(), Box<dyn std::error::Error>>;
+    ) -> Result<Response, Box<dyn std::error::Error>>;
     async fn register_device(
         &self,
         client_info: &Contact,
-    ) -> Result<(), Box<dyn std::error::Error>>;
-    async fn send_msg(&self, msg: String) -> Result<(), Box<dyn std::error::Error>>;
-    async fn update_client(&self, new_client: &Contact) -> Result<(), Box<dyn std::error::Error>>;
-    async fn delete_client(&self, uuid: String) -> Result<(), Box<dyn std::error::Error>>;
-    async fn delete_device(&self, uuid: String) -> Result<(), Box<dyn std::error::Error>>;
+    ) -> Result<Response, Box<dyn std::error::Error>>;
+    async fn send_msg(
+        &self,
+        msg: String,
+        uuid: String,
+    ) -> Result<Response, Box<dyn std::error::Error>>;
+    async fn update_client(
+        &self,
+        new_client: &Contact,
+    ) -> Result<Response, Box<dyn std::error::Error>>;
+    async fn delete_client(&self, uuid: String) -> Result<Response, Box<dyn std::error::Error>>;
+    async fn delete_device(&self, uuid: String) -> Result<Response, Box<dyn std::error::Error>>;
     fn new() -> Result<ServerAPI, Box<dyn std::error::Error>>;
 }
 
@@ -53,70 +62,84 @@ impl Server for ServerAPI {
         println!("connected!");
     }
     async fn publish_bundle(
-        &self, /*, registration_id: u32, bundle: &PreKeyBundle*/
-    ) -> Result<(), Box<dyn std::error::Error>> {
+        &self,
+        uuid: String, /*, registration_id: u32, bundle: &PreKeyBundle*/
+    ) -> Result<Response, Box<dyn std::error::Error>> {
         let payload = json!({
             "key1": "value1"
         });
-
-        self.make_request(ReqType::Post(payload), BUNDLE_URI).await
+        let uri = format!("{}/{}", BUNDLE_URI, uuid);
+        self.make_request(ReqType::Post(payload), uri).await
     }
-    async fn fetch_bundle(&self) -> Result<(), Box<dyn std::error::Error>> {
-        self.make_request(ReqType::Get, BUNDLE_URI).await
+    async fn fetch_bundle(&self, uuid: String) -> Result<Response, Box<dyn std::error::Error>> {
+        let uri = format!("{}/{}", BUNDLE_URI, uuid);
+        self.make_request(ReqType::Get, uri).await
     }
 
     async fn register_client(
         &self,
         client_info: &Contact,
-    ) -> Result<(), Box<dyn std::error::Error>> {
+    ) -> Result<Response, Box<dyn std::error::Error>> {
         let payload = json!({
-            "uuid": client_info.uuid // TODO: Change this to username or aci when implemented.
+            "aci": client_info.uuid // TODO: Change this to username or aci when implemented.
         });
 
-        self.make_request(ReqType::Post(payload), CLIENT_URI).await
+        self.make_request(ReqType::Post(payload), CLIENT_URI.to_string())
+            .await
     }
 
     async fn register_device(
         &self,
         client_info: &Contact,
-    ) -> Result<(), Box<dyn std::error::Error>> {
+    ) -> Result<Response, Box<dyn std::error::Error>> {
         let payload = json!({
             "uuid": client_info.uuid
         });
+        let uri = format!("{}/{}", DEVICE_URI, client_info.uuid);
 
-        self.make_request(ReqType::Post(payload), DEVICE_URI).await
+        self.make_request(ReqType::Post(payload), uri).await
     }
 
-    async fn send_msg(&self, msg: String) -> Result<(), Box<dyn std::error::Error>> {
+    async fn send_msg(
+        &self,
+        msg: String,
+        uuid: String,
+    ) -> Result<Response, Box<dyn std::error::Error>> {
         let payload = json!({
             "message": msg
         });
+        let uri = format!("{}/{}", MSG_URI, uuid);
 
-        self.make_request(ReqType::Put(payload), MSG_URI).await
+        self.make_request(ReqType::Put(payload), uri).await
     }
 
-    async fn update_client(&self, client: &Contact) -> Result<(), Box<dyn std::error::Error>> {
+    async fn update_client(
+        &self,
+        client: &Contact,
+    ) -> Result<Response, Box<dyn std::error::Error>> {
         let payload = json!({
             "uuid": client.uuid
         });
-        self.make_request(ReqType::Put(payload), CLIENT_URI).await
-    }
-
-    async fn delete_client(&self, uuid: String) -> Result<(), Box<dyn std::error::Error>> {
-        let payload = json!({
-            "uuid": uuid
-        });
-
-        self.make_request(ReqType::Delete(payload), CLIENT_URI)
+        self.make_request(ReqType::Put(payload), CLIENT_URI.to_string())
             .await
     }
 
-    async fn delete_device(&self, uuid: String) -> Result<(), Box<dyn std::error::Error>> {
+    async fn delete_client(&self, uuid: String) -> Result<Response, Box<dyn std::error::Error>> {
         let payload = json!({
             "uuid": uuid
         });
-        self.make_request(ReqType::Delete(payload), DEVICE_URI)
-            .await
+        let uri = format!("{}/{}", CLIENT_URI, uuid);
+
+        self.make_request(ReqType::Delete(payload), uri).await
+    }
+
+    async fn delete_device(&self, uuid: String) -> Result<Response, Box<dyn std::error::Error>> {
+        let payload = json!({
+            "uuid": uuid
+        });
+        let uri = format!("{}/{}", DEVICE_URI, uuid);
+
+        self.make_request(ReqType::Delete(payload), uri).await
     }
 
     fn new() -> Result<ServerAPI, Box<dyn std::error::Error>> {
@@ -134,8 +157,8 @@ impl ServerAPI {
     async fn make_request(
         &self,
         req_type: ReqType,
-        uri: &str,
-    ) -> Result<(), Box<dyn std::error::Error>> {
+        uri: String,
+    ) -> Result<Response, Box<dyn std::error::Error>> {
         let res = match req_type {
             ReqType::Get => self.client.get(uri),
             ReqType::Post(payload) => self.client.post(uri).body(surf::Body::from_json(&payload)?),
@@ -148,7 +171,7 @@ impl ServerAPI {
         .await?;
 
         match res.status() {
-            StatusCode::Ok => Ok(()),
+            StatusCode::Ok => Ok(res),
             _ => Err("Failed".into()),
         }
     }
