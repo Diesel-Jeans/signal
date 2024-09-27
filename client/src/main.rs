39046de94c27fd4a8501da23efe1668a9e90561e
Simon Deleuran Laursen <69715502+Laursen79@users.noreply.github.com>
2024-09-27 09:48:35 +0200

    added rudementary server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,8 @@
 use contact::{Contact, Device};
 use libsignal_protocol::*;
 use rand::rngs::OsRng;
+use surf::http::mime::JSON;
+use surf::http::Response;
 
 mod client;
 mod client_common;
@@ -10,6 +12,7 @@ mod server;
 use crate::client::Client;
 
 use common::signal_protobuf::Envelope;
+use common::signal_protocol_messages::RegistrationRequest;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -68,8 +71,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         report_spam_token: None,
         shared_mrm_key: None,
     };
+    /*
 
-    surf::put("http://127.0.0.1:50051/messages")
+    surf::put("http://127.0.0.1:50051/message/bob")
         .body_json(&envelope)?
         .await?;
 
@@ -82,6 +86,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let alice_msg = String::from_utf8(alice_bytes)?;
     println!("{alice_msg}");
     //})
+     */
+    println!("Sending message: Register",);
+    let mut response = surf::post("http://127.0.0.1:50051/client")
+        .body_json(&RegistrationRequest {
+            aci: "Darkros124".to_string(),
+        })
+        .expect("should send successful request")
+        .await
+        .expect("should get successful response");
+    println!("Got response: {:?}", response.status());
 
     Ok(())
 }
