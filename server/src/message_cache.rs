ba2a1bace948e740e4be2892233cf843fcab31e2
alexsteffensen <asteff20@student.aau.dk>
2024-10-21 13:10:23 +0200

    removed env line

--- server/src/message_cache.rs
+++ server/src/message_cache.rs
@@ -66,7 +66,6 @@ impl RedisPubSubMessageListener {
 
 impl MessageCache {
     pub async fn connect() -> Result<MessageCache> {
-        dotenv::dotenv().expect("Unable to load environment variables from ..env file");
         let redis_url = std::env::var("REDIS_URL").expect("Unable to read REDIS_URL .env var");
         let mut redis_config = Config::from_url(redis_url);
         let redis_pool: deadpool_redis::Pool = redis_config.create_pool(Some(Runtime::Tokio1))?;
