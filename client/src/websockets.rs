96662454786bf13bcce1603079270c88a2ca7d23
Arthur August Osnes Gottlieb <arthurosnes@gmail.com>
2024-10-28 12:42:52 +0100

    Fuck this garbage

--- client/src/websockets.rs
+++ client/src/websockets.rs
@@ -10,6 +10,7 @@ use prost;
 use prost::encoding::hash_map::encode;
 use prost::Message as ProstMessage;
 use std::env;
+use futures_util::stream::{SplitSink, SplitStream};
 use surf::http::headers::ToHeaderValues;
 use tokio::net::TcpStream;
 use tokio::*;
@@ -19,6 +20,29 @@ use tokio_tungstenite::tungstenite::WebSocket;
 use tokio_tungstenite::*;
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 use url::Url;
+use std::sync::{Arc, Mutex};
+
+struct StreamHandler {
+    pub stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
+}
+
+impl StreamHandler {
+    async fn try_new() -> Result<StreamHandler> {
+        // let (mut write, mut read) = open_ws_connection_to_server_as_client().await?.split();
+        // Ok(StreamHandler { write, read })
+        let stream = open_ws_connection_to_server_as_client().await?;
+        Ok(StreamHandler { stream })
+    }
+}
+
+pub(crate) struct WebsocketHandler {
+    pub(crate) socket: Arc<Mutex<StreamHandler>>,
+}
+impl WebsocketHandler {
+    pub async fn try_new() -> Result<WebsocketHandler> {
+        Ok(WebsocketHandler { socket: Arc::new(Mutex::new(StreamHandler::try_new().await?)) })
+    }
+}
 
 pub(crate) async fn send_ws_message(
     mut ws_stream: WebSocket<tungstenite::stream::MaybeTlsStream<std::net::TcpStream>>,
@@ -60,39 +84,56 @@ pub(crate) async fn open_ws_connection_to_server_as_client() -> Result<WebSocket
     Ok(ws_stream)
 }
 
+//A bit overengineered for a single certificate, but it should be kept in case more certificates are added
 fn get_certs() -> Result<Vec<Certificate>> {
-    let rootca = include_str!("../../server/cert/rootCA.crt").to_string();
-    let server = include_str!("../../server/cert/server.crt").to_string();
-
     Ok(vec![
-        Certificate::from_pem(rootca.as_bytes())?,
-        Certificate::from_pem(server.as_bytes())?,
+        Certificate::from_pem(include_str!("../../server/cert/rootCA.crt").to_string().as_bytes())?,
     ])
 }
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Deref;
     use super::*;
     use futures_util::future::ok;
+    use futures_util::{Sink, TryStream, TryStreamExt};
+    use futures_util::StreamExt;
+    use std::task::{Poll, Context};
+    use futures_util::stream::FusedStream;
+    use tokio::io::AsyncReadExt;
+
     #[tokio::test]
     async fn test_websocket() {
         dotenv::dotenv().ok();
-        open_ws_connection_to_server_as_client()
-            .await
-            .expect("Failed to connect to server");
-        // Define the WebSocket server URL
-        let mut ws_url = "wss://echo.websocket.org".into_client_request().unwrap();
 
-        // Connect to the WebSocket server
-        let (mut ws_stream, _) = tungstenite::connect(ws_url).expect("Failed to connect");
+        let handler = WebsocketHandler::try_new().await.unwrap();
+        // handler.socket.try_lock().unwrap().write.send(Message::Text("Hello, world!".to_owned())).await.unwrap();
+        // handler.socket.try_lock().unwrap().write.send(Message::Text("Hello, world!".to_owned())).await.unwrap();
 
-        println!("WebSocket connection established!");
 
-        let fuck_rust = ws_stream.read();
-        match fuck_rust {
-            Ok(msg) => println!("Got message: {}", msg),
-            Err(e) => println!("Got error: {:?}", e),
+        handler.socket.try_lock().unwrap().stream.send(Message::Text("Hello, World!!".into())).await.unwrap();
+        handler.socket.try_lock().unwrap().stream.send(Message::Text("Hello, World!".into())).await.unwrap();
+
+
+        loop {
+            println!("Looping");
+            if handler.socket.try_lock().unwrap().stream.is_terminated() || handler.socket.try_lock().unwrap().stream.size_hint() {
+                println!("Terminating loop");
+                break;
+            }
+            match handler.socket.try_lock().unwrap().stream.try_next().await.unwrap() {
+                Some(Message::Text(text)) => {
+                    println!("Received: {}", text);
+                }
+                _ => {
+                    println!("Received non-text message");
+                    break;
+                }
+            }
         }
+
+
+        // if let Ok(message) = .unwrap() {}
 
         assert!(true)
     }
