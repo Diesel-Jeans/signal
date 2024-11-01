68e8a248243ce8acfb9f10685ae035435c0f42f2
Mikkel <72790763+Mikkel-Boje@users.noreply.github.com>
2024-11-01 10:34:49 +0100

    Put keys endpoint (#77)
    * Added put keys endpoint
    * Added registration id to the [Device] struct as in Signal

--- server/src/managers/key_manager.rs
+++ server/src/managers/key_manager.rs
@@ -132,11 +132,10 @@ impl KeyManager {
             let bundle = database
                 .get_key_bundle(address)
                 .await
-                .map_err(|_| ApiError {
+                .map_err(|err| ApiError {
                     status_code: StatusCode::INTERNAL_SERVER_ERROR,
-                    message: "Could not fetch user key bundle".into(),
+                    message: format!("Could not fetch user key bundle: {}", err),
                 })?;
-
             let (pq_pre_key, signed_pre_key) = match service_id {
                 ServiceId::Aci(_) => (bundle.aci_pq_pre_key, bundle.aci_signed_pre_key),
                 ServiceId::Pni(_) => (bundle.pni_pq_pre_key, bundle.pni_signed_pre_key),
@@ -193,16 +192,18 @@ impl KeyManager {
             ProtocolAddress::new(target_service_id.service_id_string(), device.device_id())
         });
 
-        let keys = Vec::new();
+        let mut keys = Vec::new();
         for (device, ref address) in devices.iter().zip(device_addresses.into_iter()) {
-            get_key(
-                database,
-                &target_service_id,
-                address,
-                device.registration_id(),
-                device.device_id().into(),
-            )
-            .await?;
+            keys.push(
+                get_key(
+                    database,
+                    &target_service_id,
+                    address,
+                    device.registration_id(),
+                    device.device_id().into(),
+                )
+                .await?,
+            );
         }
 
         Ok(PreKeyResponse::new(
@@ -290,4 +291,238 @@ impl KeyManager {
             db.get_one_time_pq_pre_key_count(service_id).await?,
         ))
     }
+}
+
+#[cfg(test)]
+mod key_manager_tests {
+    use account::{Account, Device};
+    use common::web_api::{AccountAttributes, DeviceCapabilities};
+    use libsignal_core::{Aci, Pni};
+    use libsignal_protocol::{IdentityKey, KeyPair, PublicKey};
+    use rand::rngs::OsRng;
+    use uuid::Uuid;
+
+    use crate::postgres::PostgresDatabase;
+
+    use super::*;
+    async fn create_authenticated_device(
+        device_id: DeviceId,
+        registration_id: u32,
+        identity_key: IdentityKey,
+    ) -> AuthenticatedDevice {
+        let device = Device::new(
+            device_id,
+            "test".to_owned(),
+            0,
+            0,
+            Vec::<u8>::new(),
+            "salt".to_owned(),
+            registration_id,
+        );
+        let device_capabilities = DeviceCapabilities {
+            storage: false,
+            transfer: false,
+            payment_activation: false,
+            delete_sync: false,
+            versioned_expiration_timer: false,
+        };
+        let account_attr = AccountAttributes {
+            fetches_messages: false,
+            registration_id: registration_id.try_into().unwrap(),
+            pni_registration_id: registration_id.try_into().unwrap(),
+            capabilities: device_capabilities,
+            unidentified_access_key: <Box<[u8]>>::default(),
+        };
+        let account = Account::new(
+            Pni::from(Uuid::new_v4()),
+            device.clone(),
+            identity_key,
+            identity_key,
+            "12354678".to_owned(),
+            account_attr,
+        );
+        AuthenticatedDevice::new(account, device)
+    }
+
+    async fn add_target<S: SignalDatabase>(
+        database: &S,
+        target_device_id: &DeviceId,
+        target_service_id: &Pni,
+        target_identity_key: &IdentityKey,
+    ) -> () {
+        let device_capabilities = DeviceCapabilities {
+            storage: false,
+            transfer: false,
+            payment_activation: false,
+            delete_sync: false,
+            versioned_expiration_timer: false,
+        };
+        let account_attr = AccountAttributes {
+            fetches_messages: false,
+            registration_id: 1,
+            pni_registration_id: 1,
+            capabilities: device_capabilities,
+            unidentified_access_key: <Box<[u8]>>::default(),
+        };
+
+        let device = Device::new(
+            target_device_id.clone(),
+            "name".into(),
+            0,
+            0,
+            "no token".into(),
+            "no salt".into(),
+            1,
+        );
+
+        database
+            .add_account(&Account::new(
+                target_service_id.clone(),
+                device,
+                target_identity_key.clone(),
+                target_identity_key.clone(),
+                Uuid::new_v4().to_string().into(),
+                account_attr,
+            ))
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    async fn get_keys_test() {
+        let km = KeyManager::new();
+        let database = PostgresDatabase::connect("DATABASE_URL_TEST".into())
+            .await
+            .unwrap();
+
+        let target_device_id = DeviceId::from(111);
+        let target_service_id = Pni::from(Uuid::new_v4());
+        let identity_key = IdentityKey::from(KeyPair::generate(&mut OsRng).public_key);
+        add_target(
+            &database,
+            &target_device_id,
+            &target_service_id,
+            &identity_key,
+        )
+        .await;
+
+        let key_bundle = DevicePreKeyBundle {
+            aci_signed_pre_key: UploadSignedPreKey {
+                key_id: 1,
+                public_key: Box::new([1, 2, 3, 4]),
+                signature: Box::new([1, 2, 3, 4]),
+            },
+            pni_signed_pre_key: UploadSignedPreKey {
+                key_id: 1,
+                public_key: Box::new([1, 2, 3, 4]),
+                signature: Box::new([1, 2, 3, 4]),
+            },
+            aci_pq_pre_key: UploadSignedPreKey {
+                key_id: 1,
+                public_key: Box::new([1, 2, 3, 4]),
+                signature: Box::new([1, 2, 3, 4]),
+            },
+            pni_pq_pre_key: UploadSignedPreKey {
+                key_id: 1,
+                public_key: Box::new([1, 2, 3, 4]),
+                signature: Box::new([1, 2, 3, 4]),
+            },
+        };
+        let target_address =
+            &ProtocolAddress::new(target_service_id.service_id_string(), target_device_id);
+        database
+            .store_key_bundle(&key_bundle, target_address)
+            .await
+            .unwrap();
+
+        let one_time = UploadPreKey {
+            key_id: 1,
+            public_key: Box::new([1, 2, 3, 4]),
+        };
+        database
+            .store_one_time_ec_pre_keys(vec![one_time.clone()], target_address)
+            .await
+            .unwrap();
+
+        let auth_device1 = create_authenticated_device(0.into(), 0, identity_key).await;
+
+        let keys = km
+            .handle_get_keys(
+                &database,
+                &auth_device1,
+                target_service_id.into(),
+                Some(target_device_id),
+            )
+            .await
+            .unwrap();
+
+        let device_bundle = keys.keys();
+
+        database
+            .delete_account(&target_service_id.into())
+            .await
+            .unwrap();
+
+        assert_eq!(keys.identity_key().clone(), identity_key);
+        assert!(device_bundle.len() == 1);
+
+        assert_eq!(device_bundle[0].device_id().clone(), target_device_id);
+        assert_eq!(device_bundle[0].registration_id(), 1);
+        assert_eq!(device_bundle[0].pre_key().clone(), one_time);
+        assert_eq!(
+            device_bundle[0].signed_pre_key().clone(),
+            key_bundle.pni_signed_pre_key
+        );
+        assert_eq!(
+            device_bundle[0].pq_pre_key().clone(),
+            key_bundle.pni_pq_pre_key
+        );
+    }
+    #[tokio::test]
+    async fn put_keys_test() {
+        let km = KeyManager::new();
+        let database = PostgresDatabase::connect("DATABASE_URL_TEST".into())
+            .await
+            .unwrap();
+
+        let target_device_id = DeviceId::from(112);
+        let target_service_id = Pni::from(Uuid::new_v4());
+        let identity_key = IdentityKey::from(KeyPair::generate(&mut OsRng).public_key);
+        let auth_device = create_authenticated_device(target_device_id, 0, identity_key).await;
+
+        let prekey = UploadPreKey {
+            key_id: 1,
+            public_key: Box::new([1, 2, 3, 4]),
+        };
+        let signed_pre_key = UploadSignedPreKey {
+            key_id: 1,
+            public_key: Box::new([1, 2, 3, 4]),
+            signature: Box::new([1, 2, 3, 4]),
+        };
+        let pq_pre_key = UploadSignedPreKey {
+            key_id: 1,
+            public_key: Box::new([1, 2, 3, 4]),
+            signature: Box::new([1, 2, 3, 4]),
+        };
+        let pq_last_resort_pre_key = UploadSignedPreKey {
+            key_id: 1,
+            public_key: Box::new([1, 2, 3, 4]),
+            signature: Box::new([1, 2, 3, 4]),
+        };
+
+        let request = SetKeyRequest {
+            pre_key: Some(vec![prekey]),
+            signed_pre_key: Some(signed_pre_key),
+            pq_pre_key: Some(vec![pq_pre_key]),
+            pq_last_resort_pre_key: Some(pq_last_resort_pre_key),
+        };
+
+        km.handle_put_keys(&database, &auth_device, request, ServiceIdKind::Aci)
+            .await
+            .unwrap();
+
+        database.
+    }
+    #[tokio::test]
+    async fn check_keys_test() {}
 }
