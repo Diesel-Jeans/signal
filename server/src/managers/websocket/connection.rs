ba2ba39b884728576fa66c0ffcb0c35136457016
Arthur August Osnes Gottlieb <arthurosnes@gmail.com>
2024-11-19 08:57:28 +0100

    Now with tests

--- server/src/managers/websocket/connection.rs
+++ server/src/managers/websocket/connection.rs
@@ -48,7 +48,7 @@ pub struct WebSocketConnection<W: WSStream + Debug, DB: SignalDatabase> {
 }
 
 impl<W: WSStream + Debug + Send + 'static, DB: SignalDatabase + Send + 'static>
-    WebSocketConnection<W, DB>
+WebSocketConnection<W, DB>
 {
     pub fn new(
         identity: UserIdentity,
@@ -244,13 +244,13 @@ impl<W: WSStream + Debug + Send + 'static, DB: SignalDatabase + Send + 'static>
                                 .parse::<Uri>()
                                 .map_err(|err| err.to_string())?,
                         )?
-                        .extract::<String>(2)?
-                        .as_str(),
+                            .extract::<String>(2)?
+                            .as_str(),
                     )
-                    .ok_or("Could not parse uri to service id")?,
+                        .ok_or("Could not parse uri to service id")?,
                     unpack_messages(request_msq.body.clone())?,
                 )
-                .await
+                    .await
             }
         };
         match res {
@@ -262,7 +262,7 @@ impl<W: WSStream + Debug + Send + 'static, DB: SignalDatabase + Send + 'static>
                         vec![],
                         Some(serde_json::to_string(&res).unwrap().as_bytes().to_vec()),
                     )?
-                    .encode_to_vec(),
+                        .encode_to_vec(),
                 ))
                 .await
                 .map_err(|err| err.to_string()),
@@ -382,7 +382,6 @@ pub(crate) mod test {
     use std::{net::SocketAddr, str::FromStr};
     use tokio::sync::mpsc::{Receiver, Sender};
     use tokio::time::sleep;
-    // use tonic::codegen::tokio_stream::StreamExt;
     use super::{UserIdentity, WebSocketConnection};
 
     fn make_envelope() -> Envelope {
@@ -518,8 +517,8 @@ pub(crate) mod test {
             "timestamp": 1730217386
         }
         "#
-        .as_bytes()
-        .to_vec();
+            .as_bytes()
+            .to_vec();
 
         client
             .on_receive(create_request(
@@ -601,8 +600,8 @@ pub(crate) mod test {
             bob_address.device_id(),
             reg_id,
         )
-        .as_bytes()
-        .to_vec();
+            .as_bytes()
+            .to_vec();
 
         alice_sender
             .send(Ok(Message::Binary(
@@ -613,7 +612,7 @@ pub(crate) mod test {
                     vec![],
                     Some(sending_msg),
                 )
-                .encode_to_vec(),
+                    .encode_to_vec(),
             )))
             .await
             .unwrap();
@@ -719,7 +718,7 @@ pub(crate) mod test {
             &state.message_cache.test_key,
             state.message_cache.get_connection().await.unwrap(),
         )
-        .await;
+            .await;
 
         assert!(msg.request.is_some());
         assert!(queue.request.is_some());
