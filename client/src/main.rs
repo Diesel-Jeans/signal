327147972c862acbcc7f565e8a2754fddab37e10
Magnus J. Harder <26723180+MJHC@users.noreply.github.com>
2025-01-22 12:51:40 +0100

    Client now checks if db exists and acts accordingly (#181)
    * client now checks if db exists and acts accordingly
    
    * fmt

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,7 @@
 use client::Client;
 use dotenv::dotenv;
 use server::SignalServer;
-use std::{env::var, error::Error, fs, path::PathBuf};
+use std::{env::var, error::Error, fs, path::Path, path::PathBuf};
 use storage::device::Device;
 
 mod client;
@@ -16,26 +16,27 @@ mod storage;
 #[cfg(test)]
 mod test_utils;
 
+fn client_db_path() -> String {
+    fs::canonicalize(PathBuf::from("./client_db".to_string()))
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap()
+        .replace("\\", "/")
+        .trim_start_matches("//?/")
+        .to_owned()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv()?;
+    let client_db_dir = client_db_path();
+    let alice_path = client_db_dir.clone() + "/alice.db";
+    let bob_path = client_db_dir + "/bob.db";
 
-    let alice_db_url = format!(
-        "sqlite://{}",
-        fs::canonicalize(PathBuf::from("./client_db".to_string()))
-            .unwrap()
-            .into_os_string()
-            .into_string()
-            .unwrap()
-    ) + "/alice.db";
-    let bob_db_url = format!(
-        "sqlite://{}",
-        fs::canonicalize(PathBuf::from("./client_db".to_string()))
-            .unwrap()
-            .into_os_string()
-            .into_string()
-            .unwrap()
-    ) + "/bob.db";
+    let alice_db_url = format!("sqlite://{}", alice_path);
+    let bob_db_url = format!("sqlite://{}", bob_path);
+
     let server_url = var("SERVER_URL").expect("Could not find SERVER_URL");
     let cert_path = var("CERT_PATH").expect("Could not find CERT_PATH");
 
@@ -43,29 +44,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .install_default()
         .expect("Failed to install rustls crypto provider");
 
-    /*
-    let mut alice = Client::<Device, SignalServer>::register(
-        "alice_device",
-        "123456789".into(),
-        &alice_db_url,
-        &server_url,
-        &cert_path,
-    )
-    .await?;
-    let mut bob = Client::<Device, SignalServer>::register(
-        "bob_device",
-        "987654321".into(),
-        &bob_db_url,
-        &server_url,
-        &cert_path,
-    )
-    .await?;
-    */
+    let mut alice = if Path::exists(Path::new(&alice_path)) {
+        Client::<Device, SignalServer>::login(&alice_db_url, &cert_path, &server_url).await?
+    } else {
+        Client::<Device, SignalServer>::register(
+            "alice_device",
+            "123456789".into(),
+            &alice_db_url,
+            &server_url,
+            &cert_path,
+        )
+        .await?
+    };
 
-    let mut alice =
-        Client::<Device, SignalServer>::login(&alice_db_url, &cert_path, &server_url).await?;
-    let mut bob =
-        Client::<Device, SignalServer>::login(&bob_db_url, &cert_path, &server_url).await?;
+    let mut bob = if Path::exists(Path::new(&bob_path)) {
+        Client::<Device, SignalServer>::login(&bob_db_url, &cert_path, &server_url).await?
+    } else {
+        Client::<Device, SignalServer>::register(
+            "bob_device",
+            "987654321".into(),
+            &bob_db_url,
+            &server_url,
+            &cert_path,
+        )
+        .await?
+    };
 
     // 1st message
     alice
